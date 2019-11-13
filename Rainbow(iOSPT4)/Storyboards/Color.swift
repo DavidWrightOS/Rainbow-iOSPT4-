3068868ed2dcc259b24f5f81bec40215da8ea360
David Wright <davidtwright@gmail.com>
2019-11-12 22:32:00 -0500

    Set up initial views

---
+++ Rainbow(iOSPT4)/Storyboards/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow(iOSPT4)
+//
+//  Created by David Wright on 11/12/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
